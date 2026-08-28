093eaba834ce3213e4c39b6355889edb6aedbb1c
senecursos <cursos.sene@gmail.com>
2026-08-28 15:39:41 -0300

    Add files via upload

---
+++ analytics.js
@@ -0,0 +1,14 @@
+// Arquivo Central de Métricas - Escale Concursos
+
+// 1. Carrega o script oficial do Google Analytics
+var script = document.createElement('script');
+script.async = true;
+script.src = 'https://www.googletagmanager.com/gtag/js?id=G-KZSQN2BW69';
+document.head.appendChild(script);
+
+// 2. Inicializa as configurações de rastreamento
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-KZSQN2BW69');
